c4c2a41850f0a1a62f6a08ba3fcd16d15ee5d21f
Saifullah Khan <92732640+saifkha09090@users.noreply.github.com>
2025-01-10 19:22:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-let arr = ["+", "-", "*", "/", "%", "."];
+let operators = ["+", "-", "*", "/", "%", "."];
 let firstNum = ["0", "*", "/", "%", "."];
 
 function calculator(val) {
   let input = document.getElementById("input");
   let lastVal = input.value.slice(-1);
-  let firstVal = input.value.slice(0, 1);
+  let getFirstVal = input.value.slice(0, 1);
 
-  if (arr.includes(lastVal) && arr.includes(val)) {
-    input.value = input.value.slice(0, -1);
-  }
-
-  if (val === "X") {
+  if (operators.includes(lastVal) && operators.includes(val)) {
+    input.value = input.value.slice(0, -1) + val;
+  }else if (val === "X") {
     input.value = input.value.slice(0, -1);
   } else if (val === "=") {
     input.value = eval(input.value);
   } else if (val === "C") {
     input.value = "";
-  } else if (firstNum.includes(firstVal)) {
+  } else if (firstNum.includes(getFirstVal)) {
     input.value = input.value.slice(1);
     input.value = val;
   } else {
